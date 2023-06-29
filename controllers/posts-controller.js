bf6c62faa7aee3b525fb820b7482790a60fdcbd0
Rustem <gladius.dancer@gmail.com>
2023-06-29 17:51:29 +0500

    Posts fix

--- controllers/posts-controller.js
+++ controllers/posts-controller.js
@@ -3,7 +3,7 @@ const postsService = require("../services/posts-service");
 class PostsController{
     async addPost(req, res, next){
         try {
-            const {user_id, text, image} = req.body;
+            const {userId, text, image} = req.body;
             const date = new Date().toString();
             const postData = await postsService.addPost(userId, text, date, image);
             return res.json(postData);
