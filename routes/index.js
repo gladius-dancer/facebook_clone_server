7f3ed59e0f68530378bc7f220cba0cfee5d3fc9c
Rustem <gladius.dancer@gmail.com>
2023-06-30 11:45:53 +0500

    Posts fix

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
+const multer = require('multer');
 const Router = require("express").Router;
 const userController = require("../controllers/user-controller");
 const postsController = require("../controllers/posts-controller");
 const commentsController = require("../controllers/comments-controller");
 const {body} = require("express-validator");
 const authMiddleware = require("../middlewares/auth-middleware");
+
+const upload = multer({ dest: 'uploads/' });
+
 
 const router  = new Router();
 
@@ -17,7 +21,7 @@ router.get("/activate/:link", userController.activate);
 router.get("/refresh", userController.refresh);
 router.get("/users", authMiddleware, userController.getUsers);
 router.get("/friends", authMiddleware, userController.getFriends);
-router.post("/add-post", authMiddleware, postsController.addPost);
+router.post("/add-post", authMiddleware, upload.single('file'), postsController.addPost);
 router.get("/posts", authMiddleware, postsController.getAllPosts);
 router.post("/add-comment", authMiddleware, commentsController.addComment);
 router.get("/comments", authMiddleware, commentsController.getAllComments);
