47e43b80cf485d3d659e1f76b85e9b67d4ec8e04
Rustem <gladius.dancer@gmail.com>
2023-07-02 11:34:02 +0500

    Login fix

--- dtos/user-dto.js
+++ dtos/user-dto.js
@@ -2,9 +2,11 @@ module.exports = class UserDto{
     email;
     id;
     isActivated;
+    firstName;
     constructor(modal) {
         this.email = modal.email;
         this.id = modal._id;
         this.isActivated = modal.isActivated;
+        this.firstName;
     }
 }
